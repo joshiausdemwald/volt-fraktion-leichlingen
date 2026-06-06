7db64a34aa723776d7276e12f3c7a7fed7b520e7
Joshiausdemwald <johannes.heinen@gmail.com>
2026-06-06 11:05:24 +0200

    changed url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   // FÜR CUSTOM DOMAIN:
   // site: 'https://volt-fraktion-leichlingen.de',
-  site: 'https://joshiausdemwald.github.io',
-  base: '/volt-fraktion-leichlingen',
+  site: 'https://zukunft-leichlingen.de',
+  base: '/',
 
   // FÜR STANDARD GITHUB PAGES SUBPATH (z.B. https://username.github.io/repo/):
   // site: 'https://<username>.github.io',
